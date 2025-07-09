b9c38f953591cd9eb878ec3d52bead892e0324c9
Trishna2005Das <trishnadas7897@gmail.com>
2025-07-10 04:58:59 +0530

    deployment-in-progress

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // 👈 important for user site (root)
   plugins: [react()],
   resolve: {
     alias: {
